a2146ec115e9bf36fc4a163be500be86b42bb4b5
manasshukla <manasshukla.official@gmail.com>
2020-06-11 17:40:03 +0530

    Week#2, Challenge 2 complete.

--- src/main/java/com/redislabs/university/RU102J/dao/SiteStatsDaoRedisImpl.java
+++ src/main/java/com/redislabs/university/RU102J/dao/SiteStatsDaoRedisImpl.java
@@ -47,7 +47,8 @@ public class SiteStatsDaoRedisImpl implements SiteStatsDao {
             ZonedDateTime day = reading.getDateTime();
             String key = RedisSchema.getSiteStatsKey(siteId, day);
 
-            updateBasic(jedis, key, reading);
+//            updateBasic(jedis, key, reading);
+            updateOptimized(jedis, key, reading);
         }
     }
 
@@ -81,6 +82,16 @@ public class SiteStatsDaoRedisImpl implements SiteStatsDao {
     // Challenge #3
     private void updateOptimized(Jedis jedis, String key, MeterReading reading) {
         // START Challenge #3
+        String reportingTime = ZonedDateTime.now(ZoneOffset.UTC).toString();
+        Transaction transaction = jedis.multi();
+        transaction.hset(key, SiteStats.reportingTimeField, reportingTime);
+        transaction.hincrBy(key, SiteStats.countField, 1);
+        transaction.expire(key, weekSeconds);
+        compareAndUpdateScript.updateIfGreater(transaction, key, SiteStats.maxWhField, reading.getWhGenerated());
+        compareAndUpdateScript.updateIfLess(transaction, key, SiteStats.minWhField, reading.getWhGenerated());
+        compareAndUpdateScript.updateIfGreater(transaction, key, SiteStats.maxCapacityField, getCurrentCapacity(reading));
+
+        transaction.exec();
         // END Challenge #3
     }
 
